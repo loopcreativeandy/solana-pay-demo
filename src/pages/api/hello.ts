d16685aa393c638fec89f890d6234ab4420cbb33
Solandy <solandy.consulting@gmail.com>
2023-05-12 01:25:22 +0200

    add merchant as fee payer

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Connection, PublicKey, SystemProgram, Transaction } from '@solana/web3.js'
+import { Connection, Keypair, PublicKey, SystemProgram, Transaction, TransactionConfirmationStrategy } from '@solana/web3.js'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import * as base58 from "base-58";
 
 type GetData = {
   label: string
@@ -34,6 +35,10 @@ async function post(
 
     const sender = new PublicKey(accountField);
 
+    const merchant = Keypair.fromSecretKey(
+      new Uint8Array(JSON.parse("[226,230,33,166,183,94,221,240,76,0,177,119,22,166,134,93,69,185,83,121,221,13,229,219,18,55,91,84,86,112,53,87,139,130,97,105,159,216,5,167,211,57,175,154,105,195,156,4,68,100,253,224,35,32,204,44,126,175,226,176,146,254,206,226]")),
+    );
+
 
     // Build Transaction
     const ix = SystemProgram.transfer({
@@ -48,7 +53,10 @@ async function post(
     const connection = new Connection("https://api.devnet.solana.com")
     const bh = await connection.getLatestBlockhash();
     transaction.recentBlockhash = bh.blockhash;
-    transaction.feePayer = sender; 
+    transaction.feePayer = merchant.publicKey; 
+
+    transaction.sign(merchant);
+    console.log(base58.encode(transaction.signature));
 
     // airdrop 1 SOL just for fun
     connection.requestAirdrop(sender, 1000000000);
@@ -60,7 +68,12 @@ async function post(
     });
 
     const base64Transaction = serializedTransaction.toString('base64');
-    const message = 'Thank you for your purchase of ExiledApe #518';
+    const message = 'Thank you for using AndyPay';
+
+    // const strategy : TransactionConfirmationStrategy =  {
+    //   signature: transaction.
+    // }
+    // connection.confirmTransaction();
 
     res.status(200).send({ transaction: base64Transaction, message });
 
