47c10e3c60462bd9b610c65a788950ac6a5f61be
Solandy <solandy.consulting@gmail.com>
2023-05-12 17:53:49 +0200

    change link to production

--- src/views/basics/index.tsx
+++ src/views/basics/index.tsx
@@ -8,7 +8,7 @@ import pkg from '../../../package.json';
 import { createQR } from "@solana/pay";
 
 
-const SOLANA_PAY_URL = "solana:https://solana-pay-demo-loopcreativeandy.vercel.app/api/mint";
+const SOLANA_PAY_URL = "solana:https://solana-pay-demo-teal.vercel.app/api/mint";
 
 export const BasicsView: FC = ({ }) => {
 
